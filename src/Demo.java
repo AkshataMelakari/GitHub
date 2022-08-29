ba25149e1622a8e416e373b2e72fb8c8f5de0f36
Dell <Dell@DESKTOP-PC7T60C>
2022-08-29 11:46:37 +0530

    [Akshata] 1st change in project

---
+++ src/Demo.java
@@ -0,0 +1,7 @@
+
+public class Demo 
+{
+	//UserName = AkshataMelakari
+	// pwd =  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
+	//URl = https://github.com/AkshataMelakari/GitHub.git
+}
---
+++ src/Sample1.java
@@ -0,0 +1,10 @@
+
+public class Sample1 
+{
+   public static void main(String[] args) 
+   {
+	  System.out.print("Akshata");
+	  
+	  System.out.println("Melakari");
+   }
+}
